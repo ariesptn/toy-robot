e0ae257e14ac6761705bd856fbc748a475e7da6b
Arie Septian <ariesptn@gmail.com>
2019-04-09 10:17:44 +0700

    test cleanup

--- test.js
+++ test.js
@@ -2,7 +2,28 @@ const chai = require('chai')
 const expect = chai.expect
 const { ToyRobot } = require('./toyRobot.js')
 
+const testResult = [
+    ['example-a.txt', `0,1,NORTH`],
+    ['example-b.txt', '0,0,WEST'],
+    ['example-c.txt', '3,3,NORTH'],
+    ['example-fall1.txt', '4,4,EAST'],
+    ['example-fall2.txt', '0,0,SOUTH'],
+    ['example-preplace.txt', '0,1,NORTH']
+]
+
+
+testResult.forEach(testElement => {
+    it('testing ' + testElement[0], () => {
+        const toyRobot = new ToyRobot(testElement[0])
+        toyRobot.execute()
+        const result = toyRobot.report()
+        expect(result).to.equal(testElement[1])
+    })
+})
+
 describe('toy robot test', () => {
+
+    /*
     it('example a', () => {
         const toyRobot = new ToyRobot('example-a.txt')
         toyRobot.execute()
@@ -44,6 +65,7 @@ describe('toy robot test', () => {
         const result = toyRobot.report()
         expect(result).to.equal(`0,1,NORTH`)
     })
+*/
 
     it("make sure robot cannot be placed out of bound", () => {
         const toyRobot = new ToyRobot('')
